6922ffbe78de342a00e40f8f2f4f79fb0c63b8b4
Andron00e <semenov.andrei.v@gmail.com>
2023-10-26 08:35:30 +0300

    updates in configs.py has been added

--- configs.py
+++ configs.py
@@ -1,5 +1,6 @@
 import os
 import torch
+import random
 import torchvision
 import numpy as np
 import pandas as pd
@@ -9,3 +10,17 @@ import torch.nn.functional as F
 import matplotlib.pyplot as plt
 import torchvision.transforms as transforms
 from torch.utils.data import Dataset, DataLoader
+
+
+class Constants:
+
+    seed = 42
+    batch_size = 32
+    lr = 1e-3
+
+
+def set_seed(seed):
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
