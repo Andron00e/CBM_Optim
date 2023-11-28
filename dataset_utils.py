ad4865e5d493f784d2e289c1015fa6f5c7765d5a
Andron00e <semenov.andrei.v@gmail.com>
2023-11-28 13:21:30 +0300

    updates in dataset_utils: created new class for CLIPDataset and provided an example of compatibility with HF, new processed functions has been added

--- dataset_utils.py
+++ dataset_utils.py
@@ -2,32 +2,47 @@ from configs import *
 from transformers import CLIPModel, CLIPProcessor, AutoTokenizer
 
 class CLIPDataset():
-    def __init__(self, list_image_path: list, list_txt: list):
+    """
+    Class which creates dataset with joint labels and images itself.
+    It is convenient to use CLIPDataset with HuggingFace.
+
+    Usage:
+        dataset = CLIPDataset(list_image_path=image_paths, list_txt=labels)
+
+    For HuggingFace compatibility:
+        hf_dataset = Dataset.from_dict({
+            "image_file_path": dataset.image_path,
+            "image": [Image.open(image_path) for image_path in dataset.image_path],
+            "labels": dataset.label
+         })
+    """
+    def __init__(self, list_image_path, list_txt):
+        """
+        Args:
+            list_image_path: list of paths to images in memory
+            list_txt: list of corresponding labels
+        """
         self.image_path = list_image_path
-        self.title = list_txt
+        self.label = list_txt
 
     def __len__(self):
-        return len(self.title)
+        return len(self.label)
 
     def __getitem__(self, idx):
-        image = Image.open(self.image_path[idx])
-        title = self.title[idx]
-        return image, title
+        image_path = self.image_path[idx]
+        image = Image.open(image_path)
+        label = self.label[idx]
+        return {"image_file_path": image_path, "image": image, "labels": label}
 
 
 def collate_fn(batch):
     return {
-        'image': [x[0] for x in batch],
-        'title': [x[1] for x in batch]
+        'pixel_values': torch.stack([x['pixel_values'] for x in batch]),
+        'labels': torch.tensor([x['labels'] for x in batch])
     }
 
-def preprocess_loader(loader, concepts: list):
-    preprocessed_batches = []
-    processor = CLIPProcessor.from_pretrained("openai/clip-vit-base-patch32")
-    for batch in tqdm(loader):
-        preprocessed_batch = preprocess_batch(batch, processor, concepts)
-        preprocessed_batches.append(preprocessed_batch)
-    return preprocessed_batches
-
-def preprocess_batch(batch, processor, concepts: list):
-    return processor(text=concepts, images=batch['image'], return_tensors="pt", padding=True), batch['title']
+def process_data(example_batch, model_name: str="openai/clip-vit-base-patch32"):
+    processor = CLIPProcessor.from_pretrained(model_name)
+    inputs = processor.image_processor([x for x in example_batch['image']], return_tensors="pt")
+    inputs['labels'] = example_batch['labels']
+    return inputs
