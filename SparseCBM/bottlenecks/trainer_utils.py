85796ffb3373a4a20ac99ba0cd87fcb22b63eeae
Andrei Semenov <67924720+Andron00e@users.noreply.github.com>
2024-02-23 12:58:29 +0100

    Update trainer_utils.py

--- SparseCBM/bottlenecks/trainer_utils.py
+++ SparseCBM/bottlenecks/trainer_utils.py
@@ -1,7 +1,4 @@
 # add configure_optimizers and bs_muls function
-# add .test method and test_loader if not None
-# test evaluate load instead of load_metric from datasets
-# rename preprocessed loader to loader
 # draw norm hists and compute norm diffs functions to be released
 
 import os
